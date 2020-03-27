fbcd7759ef8ad1254f80491c73102cc4713942c2
Devqon <s.geerts@live.nl>
2020-03-27 15:11:10 +0100

    Add a basic theme and Solution file

---
+++ OrchardCore.SelfLearning.Themes/OrchardCore.SelfLearning.CustomTheme/Manifest.cs
@@ -0,0 +1,11 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "SelfLearning CustomTheme",
+    Author = "Sander Geerts",
+    Website = "https://orchardproject.net",
+    Version = "0.0.1",
+    Description = "CustomTheme for learning purposes",
+    Tags = new [] { "Custom Theme", "Learning" },
+    BaseTheme = "TheTheme"
+)]
